87a54ff1b0c169a580532f5a60d1c50569b1164b
FRANKLILD <37359629+FRANKLILD@users.noreply.github.com>
2020-08-15 10:48:24 -0700

    Update test.java

--- LeetCode/src/other/test.java
+++ LeetCode/src/other/test.java
@@ -5,54 +5,41 @@ import java.util.HashSet;
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//		int[] a = new int[] {14, 27, 1, 4, 2, 50, 3, 1};
-//		int[] b = new int[] {2, 4, -4, 3, 1, 1, 14, 27, 50};
-//		System.out.print(solution(a, b));
 		
-		System.out.print(price(8));
+		System.out.print(sortString("aaaabbbbcccc"));
 		
 	}
 	
-    public static int solution(int[] x, int[] y) {
-        // Your code here
-        HashSet<Integer> a = new HashSet<Integer>();
-        for(int e : x){
-            a.add(e);
-        }
-        
-        HashSet<Integer> b = new HashSet<Integer>();
-        for(int e : y){
-            b.add(e);
-        }
-        
-        int count = 0;
-        for(int n : y){
-
-            if( !a.contains(n) ) count = n;
-        }
-        for(int n : x){
-
-            if( !b.contains(n) ) count = n;
-
-        }
-        return count;
-    }
+	 public static String sortString(String s) {
+	        char[] ar = s.toCharArray();
+	        int[] ls = new int[26];
+	        /**
+	            count duplicate characters by them index in array
+	        **/
+	        for(char n: ar){
+	            ++ls[n - 'a'];
+	        }     
+	        /**
+	            increase and decrease
+	        **/
+	        int count = s.length();
+	        StringBuilder newString = new StringBuilder();
+	        boolean dir = true;
+	        while(count > 0){
+	            for(int i = 0; i < 26; i++){
+	                if(dir == false) i = 25 - i; 
+	                System.out.println(ls[i] + "        " + i);
+	                if( ls[i] > 0) {
+	                    newString.append( (char)(i+'a') );
+	                    ls[i] = ls[i] -1;
+	                    count --;
+	                    //System.out.println(count);
+	                 }                
+	            }
+                System.out.println(dir);
+	            dir = !dir;
+	        }        
+	        return newString.toString(); 
+	    }
     
-    
-    public static float price( int num) {
-    	int weight = num * 765;
-    	float c = (float)weight / 500;
-    	
-    	
-    	float price = (c - 1) * 36 + 118;
-    	
-    	int sp = 238 * num;
-    	
-    	float total = price + sp;
-    	
-    	float convert = (float) (total / 5.09);
-    	
-    	return convert;
-    }
 }
